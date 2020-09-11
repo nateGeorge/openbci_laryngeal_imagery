60aa234aa380299f08822992be8bce0a7d051cb8
nateGeorge <wordsforthewise@gmail.com>
2020-09-11 13:28:37 -0600

    clean up ssvep code

--- ssvep.py
+++ ssvep.py
@@ -77,17 +77,25 @@ def ssvep_experiment(window, yes: bool=True, text_time: int=5, ssvep_time: int=5
 
 
 params = BrainFlowInputParams()
-params.ip_address = '10.0.0.220'
-params.ip_port = 6227
-
+wifi = True
 # cyton/daisy wifi is 6 https://brainflow.readthedocs.io/en/stable/SupportedBoards.html
-board = BoardShim(6, params)
+# bluetooth is 2
+if wifi:
+    params.ip_address = '10.0.0.220'
+    params.ip_port = 6227
+    board = BoardShim(6, params)
+else:  # bluetooth
+    params.serial_port = '/dev/ttyUSB0'
+    board = BoardShim(2, params)
+
+
+
+
 board.prepare_session()
 # by default stores 7.5 minutes of data; change num_samples higher for more
 # sampling rate of 1k/s, so 450k samples in buffer
 board.start_stream()
 print('streaming data')
-
 
 
 
@@ -116,6 +124,11 @@ df['frequency'] = 0
 
 for (start, end), yn in zip(times, yes_nos):
     df.loc[(df[30] > start) & (df[30] < end), 'frequency'] = frequencies[1] if yn else frequencies[0]
+    # this is equivalent:
+    # if yn:
+    #     df.loc[(df[30] > start) & (df[30] < end), 'frequency'] = frequencies[1]
+    # else:
+    #     df.loc[(df[30] > start) & (df[30] < end), 'frequency'] = frequencies[0]
 
 
 yeses = df[df['frequency'] == 20]
@@ -139,141 +152,3 @@ freqs = stftfreq(wsize=1000, sfreq=1000)
 
 # 60 and 80hz don't work -- too slow
 # 30 hz works, but above that doesn't work
-st = core.StaticPeriod()#screenHz=60)#, win=window)
-start = clock.getTime()
-cycles = 60
-for i in range(cycles):
-    # on
-    st.start(time_on)
-    square.opacity = 1
-    window.update()
-    # time.sleep(time_on)
-    st.complete()
-    # off
-    st.start(time_on)
-    square.opacity = 0
-    window.update()
-    # time.sleep(time_on)
-    st.complete()
-
-end = clock.getTime()
-print(end - start)
-print('should be', time_on * cycles * 2)
-
-
-class SSVEP(object):
-    #init sets the window(mywin), and the frequency of the flashing(frame_on, frame_off)
-    #Frame duration in seconds = 1/monitorframerate(in Hz)
-    #Thus the fastest frame rate could be 1 frame on 1 frame off
-    #which equals 2/60 == 30Hz
-    #Flash frequency = refreshrate/(frame_on+frame_off)
-
-    def __init__(self, mywin=visual.Window([800, 600]),
-                frame_on=5, frame_off=5, trialdur = 5.0, port='/dev/ttyACM0',
-                fname='SSVEP.csv', numtrials=1, waitdur=2):
-
-        self.mywin = mywin
-        self.square = visual.Rect(win=self.mywin, size=(100, 100), fillColor='white')
-        self.pattern1 = visual.GratingStim(win=self.mywin, name='pattern1',units='cm',
-                        tex=None, mask=None,
-                        ori=0, pos=[0, 0], size=10, sf=1, phase=0.0,
-                        color=[1,1,1], colorSpace='rgb', opacity=1,
-                        texRes=256, interpolate=True, depth=-1.0)
-        self.pattern2 = visual.GratingStim(win=self.mywin, name='pattern2',units='cm',
-                        tex=None, mask=None,
-                        ori=0, pos=[0, 0], size=10, sf=1, phase=0,
-                        color=[-1,-1,-1], colorSpace='rgb', opacity=1,
-                        texRes=256, interpolate=True, depth=-2.0)
-        self.fixation = visual.GratingStim(win=self.mywin, size = 0.3, pos=[0,0], sf=0, rgb=-1)
-        self.frame_on = frame_on
-        self.frame_off = frame_off
-        self.trialdur = trialdur
-        self.fname = fname
-        self.numtrials = numtrials
-        self.waitdur = waitdur
-        self.port = port
-
-    def collecting(self):
-        self.collector = csv_collector.CSVCollector(fname=self.fname, port= self.port)
-        self.collector.start()
-
-    def epoch(self, mark):
-        self.collector.tag(mark)
-
-    def stop(self):
-        self.mywin.close()
-        core.quit()
-
-
-    def start(self):
-
-        ###Testing framerate grabber###
-        print self.mywin.getActualFrameRate()
-        self.Trialclock = core.Clock()
-        self.freq = 60/(self.frame_on+self.frame_off)
-
-        #start saving data from EEG device.
-        self.collecting()
-
-        #possibly convert trialdur into frames given refresh rate (normally set at 60Hz)
-        self.framerate = self.mywin.getActualFrameRate()
-        #divison here makes it tricky
-        self.trialframes = self.trialdur/60
-        self.count = 0
-
-
-        while self.count<self.numtrials:
-
-            #reset tagging
-            self.should_tag = False
-            self.epoch(0)
-            while self.Trialclock.getTime()<self.trialdur:
-
-                #draws square and fixation on screen.
-                self.fixation.setAutoDraw(True)
-                self.pattern1.setAutoDraw(True)
-
-                """
-                ###Tagging the data with the calculated frequency###
-                Attempting to only get 1 sample tagged, however, this is hard.
-                """
-                """alternative way to tag
-                if self.should_tag == False:
-                    #self.epoch(self.freq)
-                    self.epoch(70)
-                    self.mywin.flip()
-
-                self.epoch(0)
-                self.should_tag = True
-                """
-                self.epoch(70)
-
-                for frameN in range(self.frame_on):
-                    self.mywin.flip()
-
-                #another way to change color with 1 pattern
-                #self.pattern1.color *= -1
-                self.pattern1.setAutoDraw(False)
-                self.pattern2.setAutoDraw(True)
-
-
-                for frameN in range(self.frame_off):
-                    self.mywin.flip()
-                self.pattern2.setAutoDraw(False)
-
-            self.epoch(0)
-            #clean black screen off
-            self.mywin.flip()
-            #wait certain time for next trial
-            core.wait(self.waitdur)
-            #reset clock for next trial
-            self.Trialclock.reset()
-            #count number of trials
-            self.count+=1
-
-            """
-            ###Tagging the Data at end of stimulus###
-
-    """
-        self.collector.disconnect()
-        self.stop()
