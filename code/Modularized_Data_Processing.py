a074763adfe40c5e9f514518c9ac2cb27f254b62
skuhn002 <kuhnsamuel2@gmail.com>
2021-01-10 06:25:16 -0700

    Fixed file paths to data in load_data()

--- code/Modularized_Data_Processing.py
+++ code/Modularized_Data_Processing.py
@@ -16,8 +16,8 @@ from mne.decoding import CSP
 from sklearn.pipeline import Pipeline
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-PATH1 = r"C:\Users\Owner\OneDrive - Regis University\laryngeal_data\fifs\\"
-PATH2 = r"C:\Users\words\OneDrive - Regis University\laryngeal_data\fifs\\"
+PATH1 = r"C:\Users\Owner\OneDrive - Regis University\laryngeal_data\data\fifs\\"
+PATH2 = r"C:\Users\words\OneDrive - Regis University\laryngeal_data\data\fifs\\"
 FILENAME1 = PATH1 + "BCIproject_trial-S5_raw.fif.gz"
 FILENAME2 = ""
 FILENAMES = [PATH1 + f for f in glob.glob(PATH1 + '*.raw.fif.gz')]
