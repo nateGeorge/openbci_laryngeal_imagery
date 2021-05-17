961d9aa33ed527938933cda6ad5af83793ba226b
skuhn002 <kuhnsamuel2@gmail.com>
2021-05-17 11:22:30 -0600

    Changed trial number

--- code/data_collection.py
+++ code/data_collection.py
@@ -1148,13 +1148,13 @@ def run_experiment(debug=True, SSVEP_one_win=False):
 
     instructions(window)
     example(window)
-    n_trials = 2
-    # trials(window, n_trials, n_trials, n_trials, n_trials, n_trials, n_trials, data, debug=debug, SSVEP_one_win=SSVEP_one_win)
-    trials(window, 0, n_trials, 0, 0, 0, 0, data, debug=debug, SSVEP_one_win=SSVEP_one_win)
+    n_trials = 10
+    trials(window, n_trials, n_trials, n_trials, n_trials, n_trials, n_trials, data, debug=debug, SSVEP_one_win=SSVEP_one_win)
+    # trials(window, 0, n_trials, 0, 0, 0, 0, data, debug=debug, SSVEP_one_win=SSVEP_one_win)
     window.close()
     data.stopBCI()
 
 
 if __name__ == '__main__':
-    run_experiment(debug=True, SSVEP_one_win=True)
+    run_experiment(debug=False, SSVEP_one_win=False)
     core.quit()
