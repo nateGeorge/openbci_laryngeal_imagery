447e732cacf5be025d99075ca2364c6b35192ef9
skuhn002 <kuhnsamuel2@gmail.com>
2021-10-24 13:40:49 -0800

    Switch Naming from Slide to Presenter as main object
    slide.py is now presenter.py and presenter is the main object instead of slide

--- code/testing/presenter.py
+++ code/testing/presenter.py
@@ -1,21 +1,23 @@
 # imports
+import time
 from psychopy import visual, core, event, sound
 from psychopy.event import Mouse, getKeys
 from psychopy.visual import Window
 from psychopy import gui
 
-# Slide Parameters
-class slide_params:
+# Presentation Parameters
+class presentation_params:
     def __init__(self, debug=False, auto_end=True):
         # auto_end - If True, close window automatically; If False, window must be closed elsewhere in program.
         self.debug = debug
         self.auto_end = auto_end
         return
 
-class slide:
+class presenter:
     # Object for Presenting a PsychoPy Slide with Common Features
     def __init__(self, params):
         self.params = params
+        self.n_cur_stims = 0 # the current number of stimuli on screen
         if self.params.debug == True:
             print("Slide")
 
@@ -27,11 +29,28 @@ class slide:
             self.psyPy_window.close()
 
     # Close Presentation
-    def end_present(self):
+    def end_present(self, wait=0):
+        # wait - Make Window wait wait seconds before closing
         if self.params.debug == True:
             print("End Present")
+        time.sleep(wait)
         self.psyPy_window.close()
 
-    # Add Slide (dictionary of values) to Present
-    def add_slide(self):
+    # Handle Slide Presentation
+    def present_slide(self):
+        if self.params.debug == True:
+            print("Present Slide")
+        # Decide which (if any) Stimuli to remove
+        #   Check how many current stimuli there are
+        print("Num Current Stimuli: " + str(self.n_cur_stims))
+        #   Unset AutoDraw for Stimuli to remove
+        #       subtract 1 from self.n_cur_stims for each stimulus to remove
+
+        # Decide Number of Stimuli to present (n)
+        # Loop through n Stimuli
+        #   Assign Position
+        #   Assign Style
+        #   Set AutoDraw
+        #   Add 1 to self.n_cur_stims for each stimulus added
+        # Flip Window
         pass
--- code/testing/test_me.py
+++ code/testing/test_me.py
@@ -1,8 +1,8 @@
 import dialog
-import slide
+import presenter
 
 debug = True
-test = ["SLD"] # DLG - test dialogue boxes
+test = ["PRSNT"] # DLG - test dialogue boxes
                        # SLD - test slide
 
 if "DLG" in test:
@@ -11,7 +11,8 @@ if "DLG" in test:
     DLG.define_dialog_features() # define dialog box features
     DLG.raise_dialog() # raise a dialog box
 
-if "SLD" in test:
-    SLD_params = slide.slide_params(debug=debug, auto_end=False) # set parameters for a slide
-    SLD = slide.slide(SLD_params) # instantiate a slide
-    SLD.end_present()
+if "PRSNT" in test:
+    PRSNT_params = presenter.presentation_params(debug=debug, auto_end=False) # set parameters for slide presentation
+    PRSNT = presenter.presenter(PRSNT_params) # instantiate a slide
+    PRSNT.present_slide() # manage presenting a new slide
+    PRSNT.end_present(wait=1) # end presentation with a slide method
