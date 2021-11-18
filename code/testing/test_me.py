926cf83a81115d6db69fb1402db310731763423c
skuhn002 <kuhnsamuel2@gmail.com>
2021-11-17 19:27:02 -0900

    Put Dialog Box Input into a dictionary

--- code/testing/test_me.py
+++ code/testing/test_me.py
@@ -11,13 +11,14 @@ if "DLG" in test:
     DLG.define_dialog_features() # define dialog box features
     dlg_settings = DLG.raise_dialog() # raise a dialog box
 
+    #Begin Test -----------------------------------#
+    DLG_settings = {"exp_id":dlg_settings[0], "brd_type":dlg_settings[1], "blt_port":dlg_settings[2], "ip_port":dlg_settings[3], "ip_addr":dlg_settings[4]}
+    print(DLG_settings)
+    #End Test -----------------------------------#
+
 if "PRSNT" in test:
     PRSNT_params = presenter.presentation_params(debug=debug, auto_end=False) # set parameters for slide presentation
     PRSNT = presenter.presenter(PRSNT_params, dlg_settings=dlg_settings) # instantiate a slide
-
-    #Begin Test -----------------------------------#
-    print(PRSNT.dlg_settings)
-    #End Test -----------------------------------#
 
     PRSNT.present_slide_set(set="multi-slide-test")
     PRSNT.end_present() # end presentation with a slide method
